28291b268ad9be9bc427ef1e19a5bad38c6d75ff
YuJungChae <37199929+YuJungChae@users.noreply.github.com>
2019-03-26 16:45:47 +0900

    kkma_python(v.0.1)

---
+++ ReadMe.md
@@ -0,0 +1,32 @@
+# 꼬꼬마 형태소 분석기 (KKMA) - Python Version
+
+꼬꼬마 형태소 분석기는 서울대학교 IDS 연구실에서 개발한 것으로, 입력된 문장에 대한 형태소, 품사, 관계 정보 분석하는 Java 라이브러리(org.snu.ids.ha.jar)로 배포됨 [1]. 아래의 그림은 kkma 홈페이지에서 확인 할 수 있는 입력문장 "오늘은 날씨가 참좋네."에 대한 출력 예시를 나타낸다.
+
+![](M:\PAPERS\IJHR2018\Code\kkma_python\img\kkma2.PNG)
+
+![](M:\PAPERS\IJHR2018\Code\kkma_python\img\kkma.PNG)
+
+[1]: http://kkma.snu.ac.kr/	"KKMA"
+
+
+
+# 프로그램 개발 목적
+
+이 프로그램은 "org.snu.ids.ha.jar"  라이브러리를 파이썬에서 로딩하여, 형태소, 품사, 관계 정보를 추출하는 것을 목적으로 함.
+
+
+
+## 프로그램 입출력 예시
+
+### 1) 단어, 형태소, 품사, 형태소/품사 추출
+
+![](M:\PAPERS\IJHR2018\Code\kkma_python\img\io.PNG)
+
+
+
+###2) 단어, 관계 추출
+
+![](M:\PAPERS\IJHR2018\Code\kkma_python\img\io2.PNG)
+
+
+
---
+++ kkma.py
@@ -0,0 +1,84 @@
+import jpype
+from konlpy.tag import Kkma
+
+
+class kkma(object):
+
+    BEST, MORP, POS = 0, 0, 1
+
+    def __init__(self, jar_path):
+        kkma = Kkma()
+        if not jpype.isJVMStarted():
+            jpype.startJVM(jpype.getDefaultJVMPath(), "-Dfile.encoding=UTF-8",
+                           "-Djava.class.path="+jar_path+"org.snu.ids.ha.jar")
+
+        java_pkg_ma = jpype.JPackage('org.snu.ids.ha.ma')
+        kkma_analyzer = java_pkg_ma.MorphemeAnalyzer
+        kkma_sentence = java_pkg_ma.Sentence
+        self.analyzer = kkma_analyzer()
+        self.sentence = kkma_sentence()
+
+        java_pkg_sp = jpype.JPackage('org.snu.ids.ha.sp')
+        kkma_tree = java_pkg_sp.ParseTree
+        kkma_parser = java_pkg_sp.Parser
+        self.tree = kkma_tree()
+        self.parser = kkma_parser()
+
+    def word_info(self, sentence):
+        word_list, morphs_list, poses_list, mp_list = [], [], [], []
+        results = self.__analysis__(sentence)
+        for word_info in results:
+            word_list.append(word_info.getExp())
+            morphs, poses, mps = [], [], []
+            for morph_info in word_info:
+                mp = morph_info.getSmplStr()
+                segments = mp.split('/')
+                morphs.append(segments[self.MORP])
+                poses.append(segments[self.POS])
+                mps.append(mp)
+            morphs_list.append(morphs)
+            poses_list.append(poses)
+            mp_list.append(mps)
+        return word_list, morphs_list, poses_list, mp_list
+
+    def __analysis__(self, sentence):
+        analysis = self.analyzer.analyze(sentence)
+        process = self.analyzer.postProcess(analysis)
+        result = self.analyzer.leaveJustBest(process)
+        return self.analyzer.divideToSentences(result)[self.BEST]
+
+    def relation(self, sentence):
+        word_list, relation_list =[], []
+        self.sentence = self.__analysis__(sentence)
+        self.tree = self.parser.parse(self.sentence)
+        for idx_word in range(0, self.sentence.size()):
+            for idx_edge in range(0, self.tree.getEdgeList().size()):
+                if self.tree.getEdgeList().get(idx_edge).getChildNode().getEojeol().getExp() == self.sentence.get(idx_word).getExp():
+                    word_list.append(self.sentence.get(idx_word).getExp())
+                    relation_list.append(self.tree.getEdgeList().get(idx_edge).getRelation())
+        return word_list, relation_list
+
+
+if __name__ == '__main__':
+    morph = kkma("lib/")
+    test_sentence = "오늘은 날씨가 참좋네."
+
+    w, m, p, mp = morph.word_info(test_sentence)
+    print("입력문장: {}".format(test_sentence))
+    print("-단어: {}".format(w))
+    print("-형태소: {}".format(m))
+    print("-품사: {}".format(p))
+    print("-형태소/품사: {}".format(mp))
+
+    w_idx = 0
+    print("{}번째 단어:'{}'는 형태소'{}'와 품사'{}'로 구성됨을 알 수 있음".format(w_idx, w[w_idx], m[w_idx], p[w_idx]))
+
+    w, r = morph.relation(test_sentence)
+    print("입력문장: {}".format(test_sentence))
+    print("-단어: {}".format(w))
+    print("-관계: {}".format(r))
+
+
+
+
+
